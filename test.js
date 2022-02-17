24da0659741a90293a2a5a59775a3f8204b06a36
Felix <a01201946@itesm.mx>
2022-02-17 12:19:50 -0600

    javascript notes

---
+++ test.js
@@ -0,0 +1,21 @@
+// not-typed language
+// variables, constants, console
+
+let numero_cervezas = 0;
+
+console.log(numero_cervezas) 
+console.info(numero_cervezas)
+console.warn(numero_cervezas)
+console.error(numero_cervezas)
+
+for (let i = 1; i <=10; i++){
+    console.log(i);
+}
+
+console.log(i)
+
+for (var j = 1; i <=10; i++){
+    console.log(j);
+}
+
+console.log(j)
