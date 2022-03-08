c89e6357d6e4a1cab60dea70720358fa6d0ad3bf
felix-rojas <A01201946@tec.mx>
2022-03-08 08:51:57 -0600

    making things work

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ const app = express();
 const rutas_natgas = require('./routes/natgas.routes');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/natgas', rutas_natgas);
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Test Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Test2 middleware!');
+    response.send('Next and previous worked!');
+});
+
+app.listen(3000);
