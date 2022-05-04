35df79f692c86c592739f9918469c37f752564d0
felix-rojas <A01201946@tec.mx>
2022-05-04 18:04:29 -0500

    it works now i guess

--- controllers/natgas_controller.js
+++ controllers/natgas_controller.js
@@ -5,20 +5,20 @@ exports.cerveza = (request, response, next) => {
     response.sendFile(path.join(__dirname, '..', 'views', 'cerveza_view.html'));
 };
 
-// exports.get_nuevo = (request, response, next) => {
-//     console.log('GET /capybaras/nuevo');
-//     response.render('nuevo', {nombre: 'Lalo'}); 
-// };
+exports.get_nuevo = (request, response, next) => {
+    console.log('GET /capybaras/nuevo');
+    response.render('nuevo', {nombre: 'Lalo'}); 
+};
 
-// exports.post_nuevo = (request, response, next) => {
-//     console.log('POST /capybaras/nuevo');
-//     console.log(request.body);
-//     const capybara = new Capybara(request.body.nombre);
-//     capybara.save();
-//     response.redirect('/capybaras');
-// };
+exports.post_nuevo = (request, response, next) => {
+    console.log('POST /capybaras/nuevo');
+    console.log(request.body);
+    const capybara = new Capybara(request.body.nombre);
+    capybara.save();
+    response.redirect('/capybaras');
+};
 
-// exports.listar = (request, response, next) => {
-//     console.log('Ruta /capybaras');
-//     response.render('lista', {capybaras: Capybara.fetchAll()}); 
-// }
+exports.listar = (request, response, next) => {
+    console.log('Ruta /capybaras');
+    response.render('lista', {capybaras: Capybara.fetchAll()}); 
+}
